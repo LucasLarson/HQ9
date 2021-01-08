5abc18ead96eeb5e756b79e3f5517e2742fcde05
Lucas Larson <LucasLarson@riseup.net>
2021-01-08 16:53:02 -0500

    harmonize sentence capitalization

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,8 +7,8 @@ program="HQ9+"
 printf 'Welcome to %s\n  a %s production\n\n' "$(basename "$0")" "${author}"
 
 platform="$(uname)" && export platform
-printf 'platform set to %s...\n' "${platform}"
-printf 'beginning compilation...\n'
+printf 'Platform set to %s...\n' "${platform}"
+printf 'Beginning compilation...\n'
 
 # create C++20 executable for macOS, if not then for Linux
 if [[ Darwin == "${platform}" ]]; then
