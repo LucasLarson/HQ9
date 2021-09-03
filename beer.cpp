6ef1d0b3764208038bbc27b0c293548100481ff8
Lucas Larson <LucasLarson@riseup.net>
2021-09-03 16:25:00 -0400

    add comment to describe math, grammar

--- beer.cpp
+++ beer.cpp
@@ -5,10 +5,22 @@ void beer() {
     int i = 99;
 #define wall " of beer on the wall"
     while (i > 1) {
+
+        // `i` bottles of beer on the wall
+        // where `i` is 2 to 99
         printf("%d bottles%s,\n", i, wall);
+
+        // `i` bottles of beer,
+        // where `i` is the same number as above
         printf("%d bottles of beer,\n", i);
+
         printf("take 1 down, pass it around,\n");
+
+        // either
+        // `i` was 3 to 99 above and return `i bottles of beer on the wall.` or
+        // `i` was 2 and return `1 bottle\0 of beer on the wall.`
         printf("%d bottle%c%s.\n\n", i - 1, i == 2 ? '\0' : 's', wall);
+
         i--;
     }
     printf("1 bottle%s,\n1 bottle of beer,\n", wall);
