1bfc6ace6d7cd703ac67ed14e6bfaa37a93bc08e
Lucas Larson <91468+LucasLarson@users.noreply.github.com>
2021-09-21 17:04:56 -0400

    clang-format and cppcheck (#119)
    * remove redundant check for 2 or more
    
    * replace `if (i - 1 == 1)` with `if (i == 2)`
    
    * add comment to describe math, grammar
    
    * apply `clang-format` (#120)
    
    * apply `clang-format` with 79-character lines, 2-space indents
    
    * conform to cpplint `legal/copyright`
    
    add commented “copyright” line
    https://github.com/cpplint/cpplint/issues/64#issuecomment-583989314
    
    * repair comment blocks to satisfy cpplint, clang-format
    
    * conform to cpplint `build/header_guard` with `HELLOWORLD_HPP_` not `HELLO_WORLD_HPP_`
    
    * remove blank from beginning of code block
    
    * replace `unsigned long long` with `u_int64_t` (cpplint runtime/int)
    
    * repair formatting following type with shorter name (b85ed54)
    
    * remove `#endif` comment from `DEBUG` block to satisfy both cpplint, clang-format

--- beer.cpp
+++ beer.cpp
@@ -1,21 +1,44 @@
+/**
+
+  HQ9+ 🍋
+
+  Lucas Larson
+  copyright GPL v3.0+
+  https://git.io/HQ9+
+
+*/
+
 #include "beer.hpp"
 #include <iostream>
 
 void beer() {
-    int i = 99;
+  int i = 99;
+
 #define wall " of beer on the wall"
-    while (i > 1) {
-        printf("%d bottle%c%s,\n", i, i == 1 ? '\0' : 's', wall);
-        printf("%d bottle%c of beer,\n", i, i == 1 ? '\0' : 's');
-        printf("take 1 down, pass it around,\n");
-        printf("%d bottle%c%s.\n\n", i - 1, (i - 1) == 1 ? '\0' : 's', wall);
-        i--;
-    }
-    printf("1 bottle%s,\n1 bottle of beer,\n", wall);
-    printf("take 1 down and pass it around,\n");
-    printf("no more bottles%s.\n\n", wall);
-    printf("No more bottles%s,\n", wall);
-    printf("no more bottles of beer,\n");
-    printf("go to the store and buy some more,\n");
-    printf("99 bottles%s.\n", wall);
+
+  while (i > 1) {
+    // `i` bottles of beer on the wall
+    // where `i` is 2 to 99
+    printf("%d bottles%s,\n", i, wall);
+
+    // `i` bottles of beer,
+    // where `i` is the same number as above
+    printf("%d bottles of beer,\n", i);
+
+    printf("take 1 down, pass it around,\n");
+
+    // either
+    // `i` was 3 to 99 above and return `i bottles of beer on the wall.` or
+    // `i` was 2 and return `1 bottle\0 of beer on the wall.`
+    printf("%d bottle%c%s.\n\n", i - 1, i == 2 ? '\0' : 's', wall);
+
+    i--;
+  }
+  printf("1 bottle%s,\n1 bottle of beer,\n", wall);
+  printf("take 1 down and pass it around,\n");
+  printf("no more bottles%s.\n\n", wall);
+  printf("No more bottles%s,\n", wall);
+  printf("no more bottles of beer,\n");
+  printf("go to the store and buy some more,\n");
+  printf("99 bottles%s.\n", wall);
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+
+  HQ9+ 🍋
+
+  Lucas Larson
+  copyright GPL v3.0+
+  https://git.io/HQ9+
+
+*/
+
 #include <climits>
 #include <cstring>
 #include <iostream>
@@ -8,33 +18,32 @@
 #define BUFFER_SIZE INT_MAX / UCHAR_MAX - USHRT_MAX
 
 int main() {
-    char input[BUFFER_SIZE];
-    std::cin.width(BUFFER_SIZE);
-    unsigned long long plus = 0;
-    std::cout << std::string(25, '\n');
-    std::cout << "This is HQ9+" << std::endl;
-    std::cout << "interpreted by C++20" << std::endl;
-    std::cout << "  a Lucas Larson production" << std::string(3, '\n');
-    for (;;) {
-        std::cout << "↪ ";
-        std::cin >> input;
-        input[BUFFER_SIZE - 1] = '\0';
-        for (unsigned long long index = 0; index < strnlen(input, BUFFER_SIZE);
-             index++) {
-            if (input[index] == 'H') {
-                helloWorld();
-            } else if (input[index] == 'Q') {
-                printf("%s\n", input);
-            } else if (input[index] == '9') {
-                beer();
-            } else if (input[index] == '+') {
-                plus++;
+  char input[BUFFER_SIZE];
+  std::cin.width(BUFFER_SIZE);
+  u_int64_t plus = 0;
+  std::cout << std::string(25, '\n');
+  std::cout << "This is HQ9+" << std::endl;
+  std::cout << "interpreted by C++20" << std::endl;
+  std::cout << "  a Lucas Larson production" << std::string(3, '\n');
+  for (;;) {
+    std::cout << "↪ ";
+    std::cin >> input;
+    input[BUFFER_SIZE - 1] = '\0';
+    for (u_int64_t index = 0; index < strnlen(input, BUFFER_SIZE); index++) {
+      if (input[index] == 'H') {
+        helloWorld();
+      } else if (input[index] == 'Q') {
+        printf("%s\n", input);
+      } else if (input[index] == '9') {
+        beer();
+      } else if (input[index] == '+') {
+        plus++;
 #if DEBUG
-                printf("%llu\n", plus);
-#endif // DEBUG
-            } else {
-                return 1;
-            }
-        }
+        printf("%llu\n", plus);
+#endif
+      } else {
+        return 1;
+      }
     }
+  }
 }
