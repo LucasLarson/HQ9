c2a46611f6a9295896df0d20c0e460adc5e99701
Lucas Larson <LucasLarson@riseup.net>
2022-11-30 10:21:57 -0500

    avoid unbound variables
    Signed-off-by: Lucas Larson <LucasLarson@riseup.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,36 +2,36 @@
 
 author="Lucas Larson"
 program="HQ9+"
-printf 'Welcome to %s\n  a %s production\n\n' "$(basename "$0")" "${author}"
+printf 'Welcome to %s\n  a %s production\n\n' "$(basename "$0")" "${author-}"
 
 platform="$(uname)"
-printf 'Platform set to %s...\n' "${platform}"
+printf 'Platform set to %s...\n' "${platform-}"
 sleep 1
 printf 'Beginning compilation...\n\n'
 sleep 2
 
 # create C++20 executable for macOS, if not then for Linux
-if [ "${platform}" = Darwin ]; then
+if [ "${platform-}" = Darwin ]; then
   clang++ -std=c++2a --verbose -Wall -Wextra -pedantic -g -integrated-as -lm \
     -lstdc++ -O0 -pthread -save-stats -save-temps -v -fcaret-diagnostics \
     -fdiagnostics-fixit-info -fdiagnostics-parseable-fixits \
     -fdiagnostics-print-source-range-info -fdiagnostics-show-option \
     -fno-builtin -fshow-column -fshow-source-location -fstandalone-debug \
     -ftime-report ./*.cpp \
-    -o "${program}"
+    -o "${program-}"
 
-elif [ "${platform}" = Linux ]; then
+elif [ "${platform-}" = Linux ]; then
   g++ --verbose -Wall -Wextra -pedantic -save-temps -v -fgnu-tm -lm -latomic \
     -lstdc++ -g -fgnat-encodings=all ./*.cpp \
-    -o "${program}"
+    -o "${program-}"
 
 fi
 
 printf '\n\nSetting the compiled file%ss permissions...\n\n' "’"
-chmod 755 "${program}"
+chmod 755 "${program-}"
 
 # check if the program is in fact executable
-if [ -x "./${program}" ]; then
+if [ -x "./${program-}" ]; then
   printf 'Compiled program verified as executable...\n\n'
 else
 
@@ -45,8 +45,8 @@ fi
 printf 'Checking the executable%ss output...\n\n' "’"
 
 # ensure the executable contains the author’s name as a heuristic
-if grep -q "${author}" "./${program}"; then
-  printf '%s  %s interpreter bootstrapping succeeded.\n\n' "✅" "${program}"
-  printf 'Activate it by entering: ./%s\n' "${program}"
+if grep -q "${author-}" "./${program-}"; then
+  printf '%s  %s interpreter bootstrapping succeeded.\n\n' "✅" "${program-}"
+  printf 'Activate it by entering: ./%s\n' "${program-}"
   printf 'then press return.\n\n\n'
 fi
